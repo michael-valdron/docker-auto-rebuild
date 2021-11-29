8611759cbd61aa799db9d57f78fc7737af8519de
Michael Valdron <michael.valdron@gmail.com>
2021-11-29 04:12:55 -0500

    changes based on new added source.

--- cmd/auto-rebuild/main.go
+++ cmd/auto-rebuild/main.go
@@ -2,117 +2,60 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
-	"os"
-	"syscall"
 
-	"github.com/fsnotify/fsnotify"
+	"github.com/michael-valdron/docker-auto-rebuild/pkg/watcher"
 	"github.com/sevlyar/go-daemon"
 )
 
+const NAME = "auto-rebuild"
+
+type Flags struct {
+	signal *string
+	target *string
+}
+
+func createFlags() *Flags {
+	return &Flags{
+		signal: flag.String("s", "", `Send signal to the daemon:
+			quit — graceful shutdown
+			stop — fast shutdown
+			reload — reloading the configuration file`),
+		target: flag.String("t", ".", "Target directory to watch, e.g. /path/to/target"),
+	}
+}
+
+func createArgsString(flags *Flags) []string {
+	args := []string{NAME}
+
+	if *flags.target != "." {
+		args[0] = args[0] + fmt.Sprintf(" -t %s", *flags.target)
+	}
+
+	return args
+}
+
+func runWatch(stop <-chan bool, done chan<- bool, workingDir string) {
+	log.Println("- - - - - - - - - - - - - - -")
+	log.Printf("Watching '%s'...\n", workingDir)
+	watcher.Watch(stop, done)
+}
+
 func main() {
-	signal := flag.String("s", "", `Send signal to the daemon:
-        quit — graceful shutdown
-        stop — fast shutdown
-        reload — reloading the configuration file`)
-	target := flag.String("t", ".", "Target directory to watch, e.g. /path/to/target")
+	flags := createFlags()
+
 	flag.Parse()
-	daemon.AddCommand(daemon.StringFlag(signal, "quit"), syscall.SIGQUIT, termHandler)
-	daemon.AddCommand(daemon.StringFlag(signal, "stop"), syscall.SIGTERM, termHandler)
-	daemon.AddCommand(daemon.StringFlag(signal, "reload"), syscall.SIGHUP, reloadHandler)
 
-	cxt := &daemon.Context{
-		PidFileName: "sample.pid",
+	watcher.RunDaemon(&daemon.Context{
+		PidFileName: fmt.Sprintf("%s.pid", NAME),
 		PidFilePerm: 0644,
-		LogFileName: "sample.log",
+		LogFileName: fmt.Sprintf("%s.log", NAME),
 		LogFilePerm: 0640,
-		WorkDir:     *target,
+		WorkDir:     *flags.target,
 		Umask:       027,
-		Args:        []string{"[go-daemon sample]"},
-	}
-
-	if len(daemon.ActiveFlags()) > 0 {
-		d, err := cxt.Search()
-		if err != nil {
-			log.Fatal("Unable to run: ", err)
-		}
-		daemon.SendCommands(d)
-		return
-	}
-
-	d, err := cxt.Reborn()
-	if err != nil {
-		log.Fatal("Unable to run: ", err)
-	}
-	if d != nil {
-		return
-	}
-	defer cxt.Release()
-
-	log.Print("- - - - - - - - - - - - - - -")
-	log.Print("daemon started")
-
-	go watch()
-
-	err = daemon.ServeSignals()
-	if err != nil {
-		log.Printf("Error: %s", err.Error())
-	}
-
-	log.Println("daemon terminated")
-}
-
-var (
-	stop = make(chan struct{})
-	done = make(chan struct{})
-)
-
-func watch() {
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer watcher.Close()
-
-	go func() {
-		for {
-			select {
-			case <-stop:
-				return
-			case event, ok := <-watcher.Events:
-				if !ok {
-					return
-				}
-				log.Println("event:", event)
-				if event.Op&fsnotify.Write == fsnotify.Write {
-					log.Println("modified file:", event.Name)
-				}
-			case err, ok := <-watcher.Errors:
-				if !ok {
-					return
-				}
-				log.Println("error:", err)
-			}
-		}
-	}()
-
-	err = watcher.Add(".")
-	if err != nil {
-		log.Fatal(err)
-	}
-	done <- struct{}{}
-}
-
-func termHandler(sig os.Signal) error {
-	log.Println("terminating...")
-	stop <- struct{}{}
-	if sig == syscall.SIGQUIT {
-		<-done
-	}
-	return daemon.ErrStop
-}
-
-func reloadHandler(sig os.Signal) error {
-	log.Println("config reloaded")
-	return nil
+		Args:        createArgsString(flags),
+	}, func(cxt *daemon.Context, stop <-chan bool, done chan<- bool) {
+		runWatch(stop, done, cxt.WorkDir)
+	}, flags.signal)
 }
