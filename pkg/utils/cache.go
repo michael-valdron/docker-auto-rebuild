1bfff936d2e5a4d893ee0fae61fa4accd7d8b49c
Michael Valdron <michael.valdron@gmail.com>
2021-12-02 03:29:29 -0500

    docs added to cache.go

--- pkg/utils/cache.go
+++ pkg/utils/cache.go
@@ -9,19 +9,23 @@ import (
 	"github.com/muesli/cache2go"
 )
 
+// Gets the name identity for the cache.
 func GetCacheName() string {
 	return "fileHashes"
 }
 
+// Creates the cache if it does not exist else gets the active cache.
 func GetOrCreateCache() *cache2go.CacheTable {
 	return cache2go.Cache(GetCacheName())
 }
 
+// Set a value to a given key in the cache.
 func SetCacheValue(key string, value string) {
 	cache := GetOrCreateCache()
 	cache.Add(key, 10*time.Minute, value)
 }
 
+// Gets a value from the cache by a given key.
 func GetCacheValue(key string) *string {
 	cache := GetOrCreateCache()
 	var value *string = nil
@@ -39,6 +43,9 @@ func GetCacheValue(key string) *string {
 	return value
 }
 
+// Initializes the cache with files with their paths as the keys and their hashes as the values.
+//
+// See: utils.GetOrCreateFileHash
 func InitFilesCache(workingDir string) {
 	err := filepath.Walk(workingDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
