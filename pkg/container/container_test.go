63272ab94fe0295da46cee5a3f3e97a04bb605db
Michael Valdron <michael.valdron@gmail.com>
2021-12-02 01:04:56 -0500

    testing changed to match changes in container.go

--- pkg/container/container_test.go
+++ pkg/container/container_test.go
@@ -18,8 +18,25 @@ func TestGetComponentDirectories(t *testing.T) {
 }
 
 func TestIsFileInComponent(t *testing.T) {
-	assert.True(t, container.IsFileInComponent("../../examples/docker-project/hello-world", "../../examples/docker-project/hello-world/Dockerfile"))
-	assert.True(t, container.IsFileInComponent("../../examples/docker-project/nodejs", "../../examples/docker-project/nodejs/test/test.js"))
-	assert.False(t, container.IsFileInComponent("../../examples/docker-project/hello-world", "../../examples/docker-project/nodejs/test/test.js"))
-	assert.False(t, container.IsFileInComponent("../../examples/docker-project/nodejs", "../../examples/docker-project/hello-world/Dockerfile"))
+	compFileOne := container.ComponentFile{
+		ComponentPath: "../../examples/docker-project/hello-world",
+		Filename:      "../../examples/docker-project/hello-world/Dockerfile",
+	}
+	compFileTwo := container.ComponentFile{
+		ComponentPath: "../../examples/docker-project/nodejs",
+		Filename:      "../../examples/docker-project/nodejs/test/test.js",
+	}
+	compFileThree := container.ComponentFile{
+		ComponentPath: "../../examples/docker-project/hello-world",
+		Filename:      "../../examples/docker-project/nodejs/test/test.js",
+	}
+	compFileFour := container.ComponentFile{
+		ComponentPath: "../../examples/docker-project/nodejs",
+		Filename:      "../../examples/docker-project/hello-world/Dockerfile",
+	}
+
+	assert.True(t, compFileOne.IsFileInComponent())
+	assert.True(t, compFileTwo.IsFileInComponent())
+	assert.False(t, compFileThree.IsFileInComponent())
+	assert.False(t, compFileFour.IsFileInComponent())
 }
